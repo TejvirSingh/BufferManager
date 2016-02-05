c654572f589cb486f3541d582bf137f4311581b3
Tejvir Singh <tejvirsaggu@yahoo.co.in>
2016-02-04 19:13:52 -0600

    New Files added

---
+++ src/bufmgr/Clock.java
@@ -0,0 +1,78 @@
+package bufmgr;
+
+/**
+ * The "Clock" replacement policy.
+ */
+class Clock extends Replacer {
+
+  //
+  // Frame State Constants
+  //
+  protected static final int AVAILABLE = 10;
+  protected static final int REFERENCED = 11;
+  protected static final int PINNED = 12;
+
+  /** Clock head; required for the default clock algorithm. */
+  protected int head;
+
+  // --------------------------------------------------------------------------
+
+  /**
+   * Constructs a clock replacer.
+   */
+  public Clock(BufMgr bufmgr) {
+    super(bufmgr);
+
+    // initialize the frame states
+    for (int i = 0; i < frametab.length; i++) {
+      frametab[i].state = AVAILABLE;
+    }
+
+    // initialize the clock head
+    head = -1;
+
+  } // public Clock(BufMgr bufmgr)
+
+  /**
+   * Notifies the replacer of a new page.
+   */
+  public void newPage(FrameDesc fdesc) {
+    // no need to update frame state
+  }
+
+  /**
+   * Notifies the replacer of a free page.
+   */
+  public void freePage(FrameDesc fdesc) {
+    fdesc.state = AVAILABLE;
+  }
+
+  /**
+   * Notifies the replacer of a pined page.
+   */
+  public void pinPage(FrameDesc fdesc) {
+
+  }
+
+  /**
+   * Notifies the replacer of an unpinned page.
+   */
+  public void unpinPage(FrameDesc fdesc) {
+
+  }
+
+  /**
+   * Selects the best frame to use for pinning a new page.
+   * 
+   * @return victim frame number, or -1 if none available
+   */
+  public int pickVictim() {
+
+    // keep track of the number of tries
+
+    // return the victim page
+      return head;  //for compilation
+
+  } // public int pick_victim()
+
+} // class Clock extends Replacer
